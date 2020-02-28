6054852324db4c1b3166f6741920fccf5b9e7b29
Julius <juliusngigim@gmail.com>
2020-02-28 12:45:16 +0300

    Clean up book spec tests and added reservation spec file

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,13 @@ require 'rspec/core'
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-	it 'is not valid without a title' do
-		expect(Book.new(title: 'Beyond Limits')).to_be_valid
+	describe "Active Model Validations" do
+		let(:book)  { Book.new(title: 'Beyond Limit')}
+		it 'is valid with a title' do
+			expect(book).to be_valid
+		end
+		it 'is invalid without a title' do
+			expect(Book.new).to_not be_valid
+		end
 	end
 end
